74ed2d852e84641425cdaf5a83279327cb464009
Syed Mohammed Redhan Kamal <syed.k@servify.in>
2018-03-08 13:20:04 +0530

    fixed login

--- common/models/sentia-user.js
+++ common/models/sentia-user.js
@@ -9,18 +9,5 @@ var webError = {
 }
 
 module.exports = function(Sentiauser) {
-  Sentiauser.disableRemoteMethodByName("create", true);
-  Sentiauser.disableRemoteMethodByName("upsert", true);
-  Sentiauser.disableRemoteMethodByName("updateAll", true);
-  Sentiauser.disableRemoteMethodByName("updateAttributes", false);
-  Sentiauser.disableRemoteMethodByName("find", true);
-  Sentiauser.disableRemoteMethodByName("findById", true);
-  Sentiauser.disableRemoteMethodByName("findOne", true);
-  Sentiauser.disableRemoteMethodByName("deleteById", true);
-  Sentiauser.disableRemoteMethodByName("confirm", true);
-  Sentiauser.disableRemoteMethodByName("count", true);
-  Sentiauser.disableRemoteMethodByName("exists", true);
-  Sentiauser.disableRemoteMethodByName("createChangeStream", true);
-  Sentiauser.disableRemoteMethodByName("replaceOrCreate", true);
-  Sentiauser.disableRemoteMethodByName("upsertWithWhere", true);
+  
 };
